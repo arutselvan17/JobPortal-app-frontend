ef409ac26e8a6ff870e7dc430c82292c155b54c9
arutselvan17 <arutselvan1705@gmail.com>
2026-04-21 16:53:37 +0530

    Fit the Footer Error in About us

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import Navbar from '../component/Navbar'
+import Footer from '../component/Footer'
 
 export default function AboutUs() {
   return (
     <div>
         <Navbar />
         
+        
+        AboutUs
         <Footer/>
-        AboutUs</div>
+        </div>
   )
 }
